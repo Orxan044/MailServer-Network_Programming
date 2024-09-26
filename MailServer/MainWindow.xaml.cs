c510b282b891d10f6e957d4d3187005da224cfb8
Orxan Huseynov <115917125+Orxan044@users.noreply.github.com>
2024-09-26 18:37:08 +0400

    :

--- MailServer/MainWindow.xaml.cs
+++ MailServer/MainWindow.xaml.cs
@@ -1,24 +1,84 @@
-﻿using System.Text;
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailServer.Smtp;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace MailServer
+namespace MailServer;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+   // private Imap _imap = new();
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
+
+    private void BtnCompose_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+
+
+
+    private void Compose()
+    {
+
+    }
+
+    private async void BtnInbox_Click(object sender, RoutedEventArgs e)
+    {
+        //Task.Run(() => 
+        //{
+        //    //Dispatcher.Invoke(() => { ListBoxMails.Items.Clear(); });
+
+        //    if (_imap is null) return;
+
+        //    var messages = _imap.GetInbox();
+
+        //    foreach ( var message in messages )
+        //    {
+        //        Dispatcher.Invoke(() => { ListBoxMails.Items.Add(message); });
+        //    }
+        //});
+
+        var _imap = new ImapClient();
+        _imap.Connect("imap.gmail.com", 993);
+        _imap.Authenticate("logmanquliyev33@gmail.com", "rykn paby iqnf madd");
+
+
+
+        var inbox = _imap!.GetFolder("Inbox");
+        inbox.Open(FolderAccess.ReadOnly);
+
+        var ids = inbox.Search(SearchQuery.All);
+
+        //IList<string> messages = [];
+
+        foreach (var id in ids)
+        {
+            ListBoxMails.Items.Add(await inbox.GetMessageAsync(id));
+        }
+
+
+        //var inbox = _imap!.GetFolder("Inbox");
+        //inbox.Open(FolderAccess.ReadOnly);
+
+        //var ids = inbox.Search(SearchQuery.All);
+
+        //foreach (var id in ids)
+        //{
+        //    var message = inbox.GetMessage(id).Subject;
+        //    Dispatcher.Invoke(() => { ListBoxMails.Items.Add(message); });
+        //}
+
+        //var messages = _imap.GetInbox();
+
+        //foreach (var message in messages)
+        //{
+        //}
+    }
+ 
 }
---
+++ MailServer/Smtp/Imap.cs
@@ -0,0 +1,49 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using System.Net.Mail;
+
+namespace MailServer.Smtp;
+
+public class Imap
+{
+    public  ImapClient? ImapClient { get; private set; }
+    public Imap()
+    {
+        imapConnect();
+    }
+
+
+    private void imapConnect()
+    {
+        ImapClient = new();
+        ImapClient.Connect("imap.gmail.com", 993);
+        ImapClient.Authenticate("logmanquliyev33@gmail.com", "rykn paby iqnf madd");
+    }
+
+    public async void GetInbox()
+    {
+
+        
+
+        var inbox = ImapClient!.GetFolder("Inbox");
+        inbox.Open(FolderAccess.ReadOnly);
+
+        var ids = inbox.Search(SearchQuery.All);
+
+        //IList<string> messages = [];
+
+        foreach (var id in ids)
+        {
+            var message = inbox.GetMessage(id).Subject;
+            //messages.Add(message);
+        }
+
+        //return messages;
+    }
+
+
+
+
+
+}
